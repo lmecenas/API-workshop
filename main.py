ead260897152de71a756ff130ec82bc69cb0f7ad
Luiz Alberto de siebra mecenas Mecevnas <luizalbertodesiebramecenasmecevnas@MacBook-Pro-de-Luiz.local>
2023-11-13 21:39:05 -0300

    adicionando mais uma rota

--- main.py
+++ main.py
@@ -32,3 +32,10 @@ def ola_mundo():
 @app.get("/produtos")
 def listar_produtos():
     return produtos
+
+@app.get("/produtos/{id}")
+def buscar_produto(id: int):
+    for produto in produtos:
+        if produto["id"] == id:
+            return produto
+    return {"Status":404,"Mensagem": "Produto nao encontrado"}
